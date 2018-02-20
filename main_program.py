23bcb26ce3669f0018837ffbd7af7b924c9d3363
belfilip <filipmigacz@gmail.com>
2018-02-20 14:12:17 +0100

    Add new criteria in get_titles.

--- main_program.py
+++ main_program.py
@@ -1,21 +1,21 @@
 
-import data
-import display
+#import data
+#import display
 
 def get_inputs(titles):
 
     data = []
-   
+
     for parameter in titles:
         user_input = input(parameter)
         data.append(user_input)
-   
+
     return data
 
 
 def get_titles():
 
-    titles = ['easy or complicated?','vodka, whiskey or wine?','strong or soft?','sweet or sour taste?']
+    titles = ['easy, medium or hard to prepare?','main ingredient is: vodka, rum, gin, whiskey or wine?','light, medium or strong?','sweet or sour taste?']
     return titles
 
 def main():
@@ -26,7 +26,7 @@ def main():
     preference = get_inputs(get_titles())
     print(preference)
 
-  
+
 
 
 if __name__ == '__main__':
