7ff9eb24760d51adc44d468a5240361df6e29354
TheReaderOne <twardzikto@gmail.com>
2018-02-20 17:09:25 +0100

    Add choose_drink function.

--- main_program.py
+++ main_program.py
@@ -13,37 +13,36 @@ def import_data_from_file(filename = 'data.txt'):
             line_list = [x for x in line.strip().split(',')]
             data_temp.append(line_list)
 
-
     for data in range(0,len(data_temp),2):
         data_preferences.append(data_temp[data])
+
     for data in range(1,len(data_temp),2):
         data_ingredients.append(data_temp[data])
 
-
     for index in range(len(data_preferences)):
         drink_data_dict[data_preferences[index][0]] = [data_preferences[index][1:],data_ingredients[index]]
-
 
     return drink_data_dict
 
 
 def choose_drink(preference,database):
 
-    for key, value in database.items():
-        if preference in value:
-            search_name = key
+    searched_dict = {}
 
-            search_ingredients = value
+    for drink,drink_data in database.items():
+        if preference in drink_data:
+            recipe = drink_data[1]
+            searched_dict[drink] = recipe
 
-    recipe = search_ingredients[1]
-
-    print('ingredients for ' + search_name + '\n')
-    for ingredient in recipe:
-        print(ingredient)
-
-
-
-
+    print("write name of interested drink:" + '\n')
+    for key in searched_dict.keys():
+        print(key)
+    
+    pick_final_drink = input('choose drink: ')
+    print('\n' + 'ingredients for ' + pick_final_drink + ':')
+    if pick_final_drink in searched_dict.keys():
+        for ingredients in searched_dict[pick_final_drink]:
+            print(ingredients)
 
 
 def get_inputs(titles):
@@ -58,16 +57,17 @@ def get_inputs(titles):
 
 def get_titles():
 
-    titles = ['easy, medium or hard to prepare? ', 'main ingredient is: vodka, rum, gin, whiskey or tequila? ',
+    titles = ['easy, medium or hard to prepare? ', 'main ingredient is: vodka, rum, gin, whiskey or wine? ',
               'light, medium or strong? ', 'sweet or sour taste? ']
     return titles
 
 
 def main():
+    preference = ['easy','vodka','strong','sweet']
 
     print('welcome in DrinkHub')
     print("please choose your drink-type preferention: " + '\n')
-    preference = get_inputs(get_titles())
+    # preference = get_inputs(get_titles())
     choose_drink(preference,import_data_from_file())
 
 
